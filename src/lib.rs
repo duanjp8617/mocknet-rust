632bc268aba35e40aec18f45255a1b25a84d074f
duanjp8617 <duanjp8617@gmail.com>
2020-08-25 14:30:17 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// first, build a library with detailed comments, following the style of https://github.com/nayuki/QR-Code-generator
+// First, I will use the lagacy command-based approach to build up the virtual network and expose required APIs
+// Then, I will use netlink and shiplift to implement an accelarated way for building the virtual network using the exact same set of APIs
+
+
 pub mod vnet;
 
 pub mod ctbuild;
