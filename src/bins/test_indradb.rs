cc9e34d595b4cedf4cf04818e34dfd7f5d9246ac
djp <duanjp8617@gmail.com>
2020-10-15 20:30:16 +0800

    we can ping indradb

--- src/bins/test_indradb.rs
+++ src/bins/test_indradb.rs
@@ -4,9 +4,14 @@ use mocknet::autogen;
 use indradb;
 
 use std::net::ToSocketAddrs;
+use std::future::Future;
 
 use futures::AsyncReadExt;
 use futures::FutureExt;
+
+use capnp::Error as CapnpError;
+use capnp_rpc::rpc_twoparty_capnp::Side;
+use capnp_rpc::{twoparty, RpcSystem};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,9 +21,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .next()
     .expect("could not parse address");
     
-    let stream = tokio::net::TcpStream::connect(&addr).await?;
-    stream.set_nodelay(true)?;
-    let (reader, writer) = tokio_util::compat::Tokio02AsyncReadCompatExt::compat(stream).split();
-    println!("The connection is successful");
-    Ok(())
+    tokio::task::LocalSet::new().run_until(async move {
+        let stream = tokio::net::TcpStream::connect(&addr).await?;
+        stream.set_nodelay(true)?;
+        let (reader, writer) = tokio_util::compat::Tokio02AsyncReadCompatExt::compat(stream).split();
+        
+        let rpc_network = Box::new(twoparty::VatNetwork::new(
+            reader,
+            writer,
+            Side::Client,
+            Default::default(),
+        ));
+        let mut rpc_system = RpcSystem::new(rpc_network, None);
+        let client: autogen::service::Client = rpc_system.bootstrap(Side::Server);
+
+        tokio::task::spawn_local(rpc_system.map(|_| ()));
+
+        let req = client.ping_request();
+        let res = req.send().promise.await?;
+
+        if res.get().unwrap().get_ready() {
+            println!("yeah");
+            Ok(())
+        } else {
+            println!("no");
+            Ok(())
+        }
+
+    }).await
 }
