759a721e499fd4bced89c8aee80fb4311275950d
djp <duanjp8617@gmail.com>
2020-10-24 12:59:22 +0800

    finish new_message_queue

--- src/database/new_message_queue.rs
+++ src/database/new_message_queue.rs
@@ -10,38 +10,54 @@ pub mod error {
     use tokio::sync::oneshot::error as oneshot;
 
     #[derive(Debug)]
-    pub enum MsgQError {
-        MpscSendError,
-        OneshotRecvError,
+    pub enum MsgQError<E: std::error::Error> {
+        QueueDrop,
+        Inner(E),
     }
 
-
-    impl<T> From<mpsc::SendError<T>> for MsgQError {
-        fn from(_: mpsc::SendError<T>) -> MsgQError {
-            MsgQError::MpscSendError
+    // If we don't enforce E to implement StdError, then rust complains.
+    impl<E: std::error::Error, T> From<mpsc::SendError<T>> for MsgQError<E> {
+        fn from(_: mpsc::SendError<T>) -> MsgQError<E> {
+            MsgQError::<E>::QueueDrop
         }
     }
 
-    impl From<oneshot::RecvError> for MsgQError {
-        fn from(_: oneshot::RecvError) -> MsgQError {
-            MsgQError::OneshotRecvError
+    impl<E: std::error::Error> From<oneshot::RecvError> for MsgQError<E> {
+        fn from(_: oneshot::RecvError) -> MsgQError<E> {
+            MsgQError::QueueDrop
         }
     }
 
-    impl fmt::Display for MsgQError {
+    impl<E: std::error::Error> fmt::Display for MsgQError<E> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             match self {
-                MsgQError::MpscSendError => {
-                    write!(f, "message queue closed with mpsc::SendError")
+                MsgQError::QueueDrop => {
+                    write!(f, "message queue is dropped")
                 }
-                MsgQError::OneshotRecvError => {
-                    write!(f, "message queue closed with oneshot::RecvError")
+                MsgQError::Inner(e) => {
+                    write!(f, "inner error message: {}", e)
                 }
             }            
         }
     }
 
-    impl std::error::Error for MsgQError {}
+    impl<E: std::error::Error> std::error::Error for MsgQError<E> {}
+
+    // If we don't enforce E to implement StdError, then rust complains.
+    // Because MsgQError::Inner is only available for type variable E 
+    // that implement std::error::Error
+    impl<E: std::error::Error> MsgQError<E> {
+        pub fn get_inner(self) -> Option<E> {
+            match self {
+                MsgQError::Inner(e) => Some(e),
+                _ => None
+            }
+        }
+
+        pub(super) fn from_error(e: E) -> Self {
+            MsgQError::Inner(e)
+        }
+    }
 }
 
 // A message with a callback channel
@@ -80,25 +96,28 @@ impl<M, R, E> Message<M, R, E> {
     }
 }
 
-pub struct Sender<M, R> {
-    tx: UnboundedSender<Message<M, R>>,
+pub struct Sender<M, R, E> {
+    tx: UnboundedSender<Message<M, R, E>>,
 }
 
-impl<M, R> Sender<M, R> {
-    pub async fn send(&self, msg: M) -> Result<R, error::MsgQError> {
+impl<M, R, E: std::error::Error> Sender<M, R, E> {
+    pub async fn send(&self, msg: M) -> Result<R, error::MsgQError<E>> {
         let (tx, rx) = oneshot::channel();
         let msg = Message::new(msg, tx);
+        
         self.tx.send(msg)?;
-        Ok(rx.await?)
+        let res = rx.await?;
+        
+        res.map_err(|e|{error::MsgQError::from_error(e)})
     }
 }
 
-pub struct Queue<M, R> {
-    rx: UnboundedReceiver<Message<M, R>>,
+pub struct Queue<M, R, E> {
+    rx: UnboundedReceiver<Message<M, R, E>>,
 }
 
-impl<M, R> Queue<M, R> {
-    pub async fn recv(&mut self) -> Option<Message<M, R>>  {
+impl<M, R, E> Queue<M, R, E> {
+    pub async fn recv(&mut self) -> Option<Message<M, R, E>>  {
         self.rx.recv().await
     }
 
@@ -106,12 +125,12 @@ impl<M, R> Queue<M, R> {
         self.rx.close()
     }
 
-    pub fn try_recv(&mut self) -> Result<Message<M, R>, TryRecvError> {
+    pub fn try_recv(&mut self) -> Result<Message<M, R, E>, TryRecvError> {
         self.rx.try_recv()
     }
 }
 
-pub fn create<M, R>() -> (Sender<M, R>, Queue<M, R>) {
+pub fn create<M, R, E>() -> (Sender<M, R, E>, Queue<M, R, E>) {
     let (tx, rx) = unbounded_channel();
     (Sender{tx}, Queue{rx})
 }
