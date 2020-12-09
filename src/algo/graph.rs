a78b8a9108461d2906fd4e46fd62656b2fcd4f57
duanjp8617 <duanjp8617@gmail.com>
2020-12-09 12:46:37 +0800

    working on algo mod

---
+++ src/algo/graph.rs
@@ -0,0 +1,64 @@
+use std::collections::{BTreeMap, HashMap};
+
+type VertexType = i32;
+type EdgeType = i32;
+
+pub struct InMemoryGraph {
+    vertexes: HashMap<u64, VertexType>,
+    edges: BTreeMap<(u64, u64), EdgeType>, // outgoing->incoming,
+    reverse_edges: BTreeMap<(u64, u64), ()> // incoming <- outgoing
+}
+
+impl InMemoryGraph {
+    pub fn new() -> Self {
+        Self {
+            vertexes: HashMap::new(),
+            edges: BTreeMap::new(),
+            reverse_edges: BTreeMap::new(),
+        }
+    }
+
+    pub fn add_vertexes_from_json(&mut self, jv: serde_json::Value) -> Result<bool, i32> {
+        let vec: Vec<(u64, VertexType)> = serde_json::from_value(jv).unwrap();
+        for (vid, v) in vec.into_iter() {
+            if self.vertexes.contains_key(&vid) {
+                panic!("repeated vertex id");
+            }
+            else {
+                self.vertexes.insert(vid, v).unwrap();
+            }            
+        }
+        Ok(true)
+    }
+
+    pub fn add_edges_from_json(&mut self, jv: serde_json::Value) -> Result<bool, i32> {
+        let vec: Vec<((u64, u64), EdgeType)> = serde_json::from_value(jv).unwrap();
+        for ((outgoing_id, incoming_id), e) in vec.into_iter() {
+            if !self.vertexes.contains_key(&outgoing_id) {
+                panic!("vertex {} not exist", &outgoing_id);
+            }
+            if !self.vertexes.contains_key(&incoming_id) {
+                panic!("vertex {} not exist", &incoming_id);
+            }
+            if self.edges.contains_key(&(outgoing_id, incoming_id)) || self.edges.contains_key(&(incoming_id, outgoing_id)) {
+                panic!("edge ({}, {}) already exists", outgoing_id, incoming_id);
+            }
+
+            self.edges.insert((outgoing_id, incoming_id), e).unwrap();
+            self.reverse_edges.insert((incoming_id, outgoing_id), ()).unwrap();
+        }
+        Ok(true)
+    }
+
+    pub fn partition(&self, mut server_list: Vec<(u32, u32)>) -> Option<HashMap<u64, u32>> {
+        let mut res = HashMap::new();
+        for vid in self.vertexes.keys() {
+            res.insert(vid, server_list[0].0);
+            server_list.get_mut(0).unwrap().1 -= 1;
+            if server_list[0].1 == 0 {
+                server_list.pop().unwrap();
+            }
+        }
+        Some(res)
+    }
+}
--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -10,36 +10,36 @@
 // 
 
 
-struct InMemoryGraph {
+// struct InMemoryGraph {
 
-}
+// }
 
-impl InMemoryGraph {
-    // add vertexes to in memory graph using the input json string
-    fn create_vertexes_from_json() {
-        unimplemented!()
-    }
+// impl InMemoryGraph {
+//     // add vertexes to in memory graph using the input json string
+//     fn create_vertexes_from_json() {
+//         unimplemented!()
+//     }
 
-    // add edges to the inmemory graph using the input json string
-    fn create_edges_from_json() {
-        unimplemented!()
-    }
+//     // add edges to the inmemory graph using the input json string
+//     fn create_edges_from_json() {
+//         unimplemented!()
+//     }
 
-    // partition the graph using the algorithm, and the server pool,
-    // return a HashMap, containing vertex id to server mapping
-    fn partition() {
-        unimplemented!()
-    }
+//     // partition the graph using the algorithm, and the server pool,
+//     // return a HashMap, containing vertex id to server mapping
+//     fn partition() {
+//         unimplemented!()
+//     }
 
-    // sync with the database, how to do it?
-    // 1. create a vertex in the indradb
-    // 2. assign the vertex property to the indradb vertex
-    // 3. add a vertex id to vertex uuid mapping
-    // 4. create an edge in the indradb.
-    // 5. assign the edge property to the indradb edge
-    // 6. add the edge id to edge uuid mapping
-    fn sync_with_db() {
-        unimplemented!()
-    }
-}
-
+//     // sync with the database, how to do it?
+//     // 1. create a vertex in the indradb
+//     // 2. assign the vertex property to the indradb vertex
+//     // 3. add a vertex id to vertex uuid mapping
+//     // 4. create an edge in the indradb.
+//     // 5. assign the edge property to the indradb edge
+//     // 6. add the edge id to edge uuid mapping
+//     fn sync_with_db() {
+//         unimplemented!()
+//     }
+// }
+pub mod graph;
