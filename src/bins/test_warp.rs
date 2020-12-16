eece398dc5bd3d7eb7989fdb064cd0cec6b3a420
djp <duanjp8617@gmail.com>
2020-12-16 18:03:12 +0800

    update

--- src/bins/test_warp.rs
+++ src/bins/test_warp.rs
@@ -42,66 +42,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send>> {
                 }
             };
             
-            // register a test user "wtf"
-            let res = client.register_user("wtf").await?;
-            match res {
-                Succeed(_) => {
-                    println!("successfully register user wtf");
-                }
-                Fail(s) => {
-                    println!("{}", &s);
-                }
-            };
+            // // register a test user "wtf"
+            // let res = client.register_user("wtf").await?;
+            // match res {
+            //     Succeed(_) => {
+            //         println!("successfully register user wtf");
+            //     }
+            //     Fail(s) => {
+            //         println!("{}", &s);
+            //     }
+            // };
 
-            // create an emunet
-            let res = client.create_emu_net("wtf".to_string(), "wtf".to_string(), 12).await?;
-            match res {
-                Succeed(uuid) => {
-                    println!("successfulily create an emunet with id {}", uuid);
-                }
-                Fail(s) => {
-                    println!("{}", &s);
-                }
-            };
+            // // create an emunet
+            // let res = client.create_emu_net("wtf".to_string(), "wtf".to_string(), 12).await?;
+            // match res {
+            //     Succeed(uuid) => {
+            //         println!("successfulily create an emunet with id {}", uuid);
+            //     }
+            //     Fail(s) => {
+            //         println!("{}", &s);
+            //     }
+            // };
 
-            // get the emunet wtf from user wtf
-            let res = client.list_emu_net_uuid("wtf".to_string()).await?;
-            let mut wtf_emunet_uuid = indradb::util::generate_uuid_v1();
-            match res {
-                Succeed(hmap) => {
-                    wtf_emunet_uuid = hmap.get("wtf").unwrap().clone();
-                    println!("wtf emunet uuid is {}", &wtf_emunet_uuid);
-                },
-                Fail(s) => {
-                    panic!("{}", &s);
-                }
-            }
+            // // get the emunet wtf from user wtf
+            // let res = client.list_emu_net_uuid("wtf".to_string()).await?;
+            // let mut wtf_emunet_uuid = indradb::util::generate_uuid_v1();
+            // match res {
+            //     Succeed(hmap) => {
+            //         wtf_emunet_uuid = hmap.get("wtf").unwrap().clone();
+            //         println!("wtf emunet uuid is {}", &wtf_emunet_uuid);
+            //     },
+            //     Fail(s) => {
+            //         panic!("{}", &s);
+            //     }
+            // }
 
-            let res = client.get_emu_net(wtf_emunet_uuid).await?;
-            let mut emu_net = res.unwrap();
-            println!("{:?}", &emu_net);
+            // let res = client.get_emu_net(wtf_emunet_uuid).await?;
+            // let mut emu_net = res.unwrap();
+            // println!("{:?}", &emu_net);
 
-            let vertexes: Vec<_> = vec!(1,2,3,4,5).into_iter().map(|e| {
-                (e, 0)
-            }).collect();
-            let edges: Vec<_> = vec!((1,2), (1,3), (1,4), (1,5), (2,3)).into_iter().map(|e| {
-                (e, 0)
-            }).collect();
+            // let vertexes: Vec<_> = vec!(1,2,3,4,5).into_iter().map(|e| {
+            //     (e, 0)
+            // }).collect();
+            // let edges: Vec<_> = vec!((1,2), (1,3), (1,4), (1,5), (2,3)).into_iter().map(|e| {
+            //     (e, 0)
+            // }).collect();
         
-            let vertex_json = serde_json::to_value(vertexes).unwrap();
-            let edge_json = serde_json::to_value(edges).unwrap();
+            // let vertex_json = serde_json::to_value(vertexes).unwrap();
+            // let edge_json = serde_json::to_value(edges).unwrap();
         
-            let graph: InMemoryGraph<u64, u64, u64> = InMemoryGraph::from_jsons(vertex_json, edge_json).unwrap();
-            graph.dump();
+            // let graph: InMemoryGraph<u64, u64, u64> = InMemoryGraph::from_jsons(vertex_json, edge_json).unwrap();
+            // graph.dump();
 
-            let partition_result = graph.partition(emu_net.servers_mut()).unwrap(); 
-            println!("{:?}", &partition_result);
+            // let partition_result = graph.partition(emu_net.servers_mut()).unwrap(); 
+            // println!("{:?}", &partition_result);
 
-            // let ru = register_user::build_filter(client.clone());
-            // let ce = create_emunet::build_filter(client.clone());
-            // // let ie = init_emunet::build_filter(client.clone());
-            // let routes = ru.or(ce);//.or(ie);
-            // warp::serve(routes).run(([172, 16, 30, 182], 3030)).await; 
+            let ru = register_user::build_filter(client.clone());
+            let ce = create_emunet::build_filter(client.clone());
+            // let ie = init_emunet::build_filter(client.clone());
+            let routes = ru.or(ce);//.or(ie);
+            warp::serve(routes).run(([172, 16, 30, 182], 3030)).await; 
 
             Ok(())
         }
