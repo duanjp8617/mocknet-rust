bc1694344e3d1a71c0bdc6789957af6cde72d03d
djp <duanjp8617@gmail.com>
2020-12-29 19:52:23 +0800

    sync

--- src/bins/test_warp.rs
+++ src/bins/test_warp.rs
@@ -11,11 +11,11 @@ use mocknet::restful::{*};
 use mocknet::algo::in_memory_graph::{InMemoryGraph};
 use mocknet::algo::Partition;
 
-const LOCAL_ADDR: [u32; 4] = [127, 0, 0, 1];
-const LOCAL_PORT: u32 = 3030;
+const LOCAL_ADDR: [u8; 4] = [127, 0, 0, 1];
+const LOCAL_PORT: u16 = 3030;
 
-const DB_ADDR: [u32; 4] = [127, 0, 0, 1];
-const DB_PORT: u32 = 27615;
+const DB_ADDR: [u8; 4] = [127, 0, 0, 1];
+const DB_PORT: u16 = 27615;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send>> {
@@ -60,73 +60,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send>> {
             let res = client.init(sp.into_vec()).await?;
             match res {
                 Ok(_) => {
-                    println!("Successfully initialize the database.")
+                    println!("successfully initialize the database")
                 },
                 Err(s) => {
+                    // the database has been initialized, just print the error message
                     println!("{}", &s);
                 }
             };
             
-            // // register a test user "wtf"
-            // let res = client.register_user("wtf").await?;
-            // match res {
-            //     Succeed(_) => {
-            //         println!("successfully register user wtf");
-            //     }
-            //     Fail(s) => {
-            //         println!("{}", &s);
-            //     }
-            // };
-
-            // // create an emunet
-            // let res = client.create_emu_net("wtf".to_string(), "wtf".to_string(), 12).await?;
-            // match res {
-            //     Succeed(uuid) => {
-            //         println!("successfulily create an emunet with id {}", uuid);
-            //     }
-            //     Fail(s) => {
-            //         println!("{}", &s);
-            //     }
-            // };
-
-            // // get the emunet wtf from user wtf
-            // let res = client.list_emu_net_uuid("wtf".to_string()).await?;
-            // let mut wtf_emunet_uuid = indradb::util::generate_uuid_v1();
-            // match res {
-            //     Succeed(hmap) => {
-            //         wtf_emunet_uuid = hmap.get("wtf").unwrap().clone();
-            //         println!("wtf emunet uuid is {}", &wtf_emunet_uuid);
-            //     },
-            //     Fail(s) => {
-            //         panic!("{}", &s);
-            //     }
-            // }
-
-            // let res = client.get_emu_net(wtf_emunet_uuid).await?;
-            // let mut emu_net = res.unwrap();
-            // println!("{:?}", &emu_net);
-
-            // let vertexes: Vec<_> = vec!(1,2,3,4,5).into_iter().map(|e| {
-            //     (e, 0)
-            // }).collect();
-            // let edges: Vec<_> = vec!((1,2), (1,3), (1,4), (1,5), (2,3)).into_iter().map(|e| {
-            //     (e, 0)
-            // }).collect();
-        
-            // let vertex_json = serde_json::to_value(vertexes).unwrap();
-            // let edge_json = serde_json::to_value(edges).unwrap();
-        
-            // let graph: InMemoryGraph<u64, u64, u64> = InMemoryGraph::from_jsons(vertex_json, edge_json).unwrap();
-            // graph.dump();
-
-            // let partition_result = graph.partition(emu_net.servers_mut()).unwrap(); 
-            // println!("{:?}", &partition_result);
-
+            // build up the warp filters
             let ru = register_user::build_filter(client.clone());
             let ce = create_emunet::build_filter(client.clone());
             // let ie = init_emunet::build_filter(client.clone());
             let routes = ru.or(ce);//.or(ie);
-            warp::serve(routes).run(([127, 0, 0, 1], 3030)).await; 
+
+            // launch the warp server
+            warp::serve(routes).run((LOCAL_ADDR, LOCAL_PORT)).await; 
 
             Ok(())
         }
--- src/restful/register_user.rs
+++ src/restful/register_user.rs
@@ -3,16 +3,9 @@ use crate::dbnew::{Client};
 use warp::{http, Filter};
 use serde::Deserialize;
 
-// curl --location --request POST 'localhost:3030/v1/register_user' \
-// --header 'Content-Type: application/json' \
-// --header 'Content-Type: text/plain' \
-// --data-raw '{
-//     "name": "fuck"
-// }'
-
 // path/register_user/:user_name
-async fn register_user(user_name: String, db_client: Client) -> Result<impl warp::Reply, warp::Rejection> {
-    let db_response = db_client.register_user(&user_name).await;
+async fn register_user(json_msg: Json, db_client: Client) -> Result<impl warp::Reply, warp::Rejection> {
+    let db_response = db_client.register_user(&json_msg.name).await;
 
     match db_response {
         Err(e) => {
@@ -21,7 +14,7 @@ async fn register_user(user_name: String, db_client: Client) -> Result<impl warp
         Ok(query_res) => {
             match query_res {
                 Ok(_) => {
-                    Ok(warp::reply::with_status(format!("user registration succeed: {}", user_name), http::StatusCode::OK))
+                    Ok(warp::reply::with_status(format!("user registration succeed: {}", &json_msg.name), http::StatusCode::OK))
                 },
                 Err(err_msg) => {
                     Ok(warp::reply::with_status(format!("operation fail: {}", err_msg), http::StatusCode::BAD_REQUEST))
@@ -36,14 +29,6 @@ struct Json {
     name: String,
 }
 
-fn parse_json_body() -> impl Filter<Extract = (String,), Error = warp::Rejection> + Clone {
-    // When accepting a body, we want a JSON body
-    // (and to reject huge payloads)...
-    warp::body::content_length_limit(1024 * 16)
-        .and(warp::body::json())
-        .map(|req_body: Json|{req_body.name})
-}
-
 pub fn build_filter(db_client: Client) 
     -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone + Send + Sync + 'static
 {
@@ -55,7 +40,7 @@ pub fn build_filter(db_client: Client)
         .and(warp::path("v1"))
         .and(warp::path("register_user"))
         .and(warp::path::end())
-        .and(parse_json_body())
+        .and(super::parse_json_body())
         .and(db_filter)
         .and_then(register_user)
 }
