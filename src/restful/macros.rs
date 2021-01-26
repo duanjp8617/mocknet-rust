e6ff0448a4980208a15b2f107f5bf94326021da7
djp <duanjp8617@gmail.com>
2021-01-26 17:35:05 +0800

    update

--- src/restful/macros.rs
+++ src/restful/macros.rs
@@ -5,24 +5,20 @@ macro_rules! extract_response {
      $err: expr) => {
         match $resp {
             Err(e) => {
-                return Ok(
-                    warp::reply::json(&crate::restful::Response::<()>::new(
-                        false,
-                        (),
-                        format!("{}: {}", $fatal, e),
-                    ))
-                );
+                return Ok(warp::reply::json(&crate::restful::Response::<()>::new(
+                    false,
+                    (),
+                    format!("{}: {}", $fatal, e),
+                )));
             }
             Ok(query_resp) => match query_resp {
                 Ok(inner) => inner,
                 Err(err_msg) => {
-                    return Ok(
-                        warp::reply::json(&crate::restful::Response::<()>::new(
-                            false,
-                            (),
-                            format!("{}: {}", $err, err_msg),
-                        ))
-                    );
+                    return Ok(warp::reply::json(&crate::restful::Response::<()>::new(
+                        false,
+                        (),
+                        format!("{}: {}", $err, err_msg),
+                    )));
                 }
             },
         }
