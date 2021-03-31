e215d01ba81ed0ba7fc260d989ec1c66f30fce6d
duanjp8617 <duanjp8617@gmail.com>
2021-03-31 15:50:21 +0000

    remove unused files

--- src/autogen.rs
+++
@@ -1,9 +0,0 @@
-#![cfg_attr(feature = "cargo-clippy", allow(clippy::wrong_self_convention))]
-include!(concat!(
-    env!("OUT_DIR"),
-    "/rpc_proto/capnp/indradb_capnp.rs"
-));
-
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
-}
