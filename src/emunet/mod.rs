264904e96b39bfbed44a0e2160cbd93eebd8f3e8
duanjp8617 <duanjp8617@gmail.com>
2021-04-02 11:13:20 +0000

    add grpc interface

--- src/emunet/mod.rs
+++ src/emunet/mod.rs
@@ -9,3 +9,8 @@ pub(crate) use cluster::ServerInfo;
 pub(crate) use device::*;
 pub(crate) use emunet::*;
 pub(crate) use user::User;
+
+mod test;
+use test::mocknet_proto::EmunetReq;
+use test::mocknet_proto::EmunetResp;
+use test::mocknet_proto::mocknet_client::MocknetClient;
